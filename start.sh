cdb50fb6e8025db1362e094f7260c1977ffadaa6
Kelaun <kelaun@kelaun.be>
2020-02-03 11:42:44 +0100

    Started rework based on Arch

--- start.sh
+++ start.sh
@@ -1,20 +1,12 @@
-#######################################
-# Author:        Kevin Andriessens    #
-# Creation date: 12th of October 2019 #
-# Last modified: 12th of October 2019 #
-# Version:       0.1                  #
-#######################################
+########################################
+# Author:        Kevin Andriessens     #
+# Creation date: 12th of October 2019  #
+# Last modified: 03rd of February 2020 #
+# Version:       1.0                   #
+########################################
 
 ## Updating the system
-sudo dnf update
+sudo pacman -Syyuu
 
-## Installing puppet
-sudo dnf install puppet
-
-## Installing pdk
-sudo rpm -Uvh https://yum.puppet.com/puppet-tools-release-fedora-31.noarch.rpm
-sudo dnf install pdk
-
-## Cloning git puppet db
-su -c "rm -rf /etc/puppet; cd /etc/; git clone https://github.com/kelaun/puppet"
-#/!\ Keep in mind you'll need to start a new branch to adapt the code where needed /!\
+## Updating to pulseaudio
+install_pulse

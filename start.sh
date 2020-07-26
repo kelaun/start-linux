957066b2e7920ab62ef948dfec6b3b198e1e51f4
Kelaun <kelaun@kelaun.be>
2020-07-26 16:25:18 +0200

    simplicity

--- start.sh
+++ start.sh
@@ -5,8 +5,17 @@
 # Version:       1.0                   #
 ########################################
 
+## Setting up the mirrorlist
+sudo pacman-mirrors -a -P https
+
 ## Updating the system
 sudo pacman -Syyuu
+
+## Installing puppet
+yay -S puppet
+
+## Cloning puppet repo
+su -c "cd /etc; rm -rf puppet; git clone https://github.com/kelaun/puppet.git;"
 
 ## Updating to pulseaudio
 install_pulse

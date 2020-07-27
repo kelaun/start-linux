168bf9e44684a5c113179785bce00f3a64261ebc
Kelaun <kelaun@kelaun.be>
2020-07-27 10:09:26 +0200

    Switching to Elementary OS

--- start.sh
+++ start.sh
@@ -1,21 +1,15 @@
 ########################################
 # Author:        Kevin Andriessens     #
 # Creation date: 12th of October 2019  #
-# Last modified: 03rd of February 2020 #
+# Last modified: 27th of July 2020     #
 # Version:       1.0                   #
 ########################################
 
-## Setting up the mirrorlist
-sudo pacman-mirrors -a -P https
-
 ## Updating the system
-sudo pacman -Syyuu
+sudo apt update && sudo apt upgrade && sudo apt dist-upgrade
 
 ## Installing puppet
-yay -S puppet
+sudo apt install puppet
 
 ## Cloning puppet repo
 su -c "cd /etc; rm -rf puppet; git clone https://github.com/kelaun/puppet.git;"
-
-## Updating to pulseaudio
-install_pulse

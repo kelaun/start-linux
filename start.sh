d3812a48d483c41bcc663ac6e0f020c1828fdc2e
Kelaun <kelaun@kelaun.be>
2020-07-27 10:46:06 +0200

    Revert "Switching to Elementary OS"
    This reverts commit 168bf9e44684a5c113179785bce00f3a64261ebc.

--- start.sh
+++ start.sh
@@ -1,15 +1,21 @@
 ########################################
 # Author:        Kevin Andriessens     #
 # Creation date: 12th of October 2019  #
-# Last modified: 27th of July 2020     #
+# Last modified: 03rd of February 2020 #
 # Version:       1.0                   #
 ########################################
 
+## Setting up the mirrorlist
+sudo pacman-mirrors -a -P https
+
 ## Updating the system
-sudo apt update && sudo apt upgrade && sudo apt dist-upgrade
+sudo pacman -Syyuu
 
 ## Installing puppet
-sudo apt install puppet
+yay -S puppet
 
 ## Cloning puppet repo
 su -c "cd /etc; rm -rf puppet; git clone https://github.com/kelaun/puppet.git;"
+
+## Updating to pulseaudio
+install_pulse

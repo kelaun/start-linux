b60c339596cf5242a2f204fdd0235781988f6811
Kelaun <kevin@kelaun.be>
2020-09-09 10:15:26 +0200

    Removing unneeded comments

--- start.sh
+++ start.sh
@@ -1,21 +1,15 @@
 ########################################
 # Author:        Kevin Andriessens     #
 # Creation date: 12th of October 2019  #
-# Last modified: 03rd of February 2020 #
-# Version:       1.0                   #
+# Last modified: 19th of August 2020   #
+# Version:       2.0                   #
 ########################################
-
-## Setting up the mirrorlist
-sudo pacman-mirrors -a -P https
 
 ## Updating the system
 sudo pacman -Syyuu
 
 ## Installing puppet
-yay -S puppet
+yay -S puppet pdk
 
 ## Cloning puppet repo
 su -c "cd /etc; rm -rf puppet; git clone https://github.com/kelaun/puppet.git;"
-
-## Updating to pulseaudio
-install_pulse
